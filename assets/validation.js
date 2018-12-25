8c6cc617860582bca8a6ca679ff77ee46c7f6b46
Rana Raheel Tariq <rana.raheel104@gmail.com>
2018-12-25 23:47:23 +0500

    Intial

--- assets/validation.js
+++ assets/validation.js
@@ -1,3 +1,15 @@
+function vali1(){
+    if(isNaN(document.getElementById("number1").value) || document.getElementById("number1")==='' ){
+        document.getElementById("error").innerHTML = "<b style='color:red'>Enter Only Numbers</b>";
+        document.getElementsByTagName("input")[0].setAttribute("type", "number");
+    }
+}
+function vali2(){
+    if(isNaN(document.getElementById("number2").value) || document.getElementById("number2")==='' ){
+        document.getElementById("error1").innerHTML = "<b style='color:red'>Enter Only Numbers</b>";
+        document.getElementsByTagName("input")[1].setAttribute("type", "number");
+    }
+}
 function add() {
     num1 = parseFloat(document.getElementById("number1").value);
     num2 = parseFloat(document.getElementById("number2").value);
@@ -8,5 +20,5 @@ function sub() {
     num1 = parseFloat(document.getElementById("number1").value);
     num2 = parseFloat(document.getElementById("number2").value);
     total = num1 - num2;
-    cal.getElementById("result").value = total;
+    document.getElementById("result").value = total;
 }
